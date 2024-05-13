a298f8d49ec8b5c9b8ecfe58c4965dc9d121ad64
Matheus Vieira <matheus.fvp@outlook.com>
2024-05-13 10:03:50 -0300

    feat: infrastructure layer - JpaUserRepository and UserEntity

---
+++ src/main/java/edu/mfvp/javaspringecommerce/infrastructure/persistence/user/gateways/JpaUserRepositoryGateway.java
@@ -0,0 +1,7 @@
+package edu.mfvp.javaspringecommerce.infrastructure.persistence.user.gateways;
+
+import edu.mfvp.javaspringecommerce.domain.user.gateways.UserRepositoryGateway;
+
+public class JpaUserRepositoryGateway implements UserRepositoryGateway {
+
+}
