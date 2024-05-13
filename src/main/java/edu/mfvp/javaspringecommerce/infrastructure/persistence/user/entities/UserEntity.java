e8acbb820723bc25182033c451fca4958b77fc76
Matheus Vieira <matheus.fvp@outlook.com>
2024-05-13 10:45:39 -0300

    feat: H2 database, test profile and JPA

--- src/main/java/edu/mfvp/javaspringecommerce/infrastructure/persistence/user/entities/UserEntity.java
+++ src/main/java/edu/mfvp/javaspringecommerce/infrastructure/persistence/user/entities/UserEntity.java
@@ -2,13 +2,30 @@ package edu.mfvp.javaspringecommerce.infrastructure.persistence.user.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "users")
 public class UserEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
     private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
     private String email;
+
+    @Column(length = 15)
     private String phone;
+
+    @Column(nullable = false)
     private String password;
 
     public Long getId() {
