fbd38cce961306f0ee3fd9038838f7d93d30b53b
Matheus Vieira <matheus.fvp@outlook.com>
2024-05-13 11:21:45 -0300

    feat: insert data into users migration

---
+++ src/main/resources/db/migration/V2__INSERT_DATA_INTO_USERS.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, phone, password) VALUES
+('João Silva', 'joao@example.com', '123456789', 'senha123'),
+('Maria Souza', 'maria@example.com', NULL, 'outrasenha456'),
+('Carlos Ferreira', 'carlos@example.com', '987654321', 'senhasecreta789');
