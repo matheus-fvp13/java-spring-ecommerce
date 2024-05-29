28071e88e999426be44d48ab5ca6c58499ed61d7
Matheus Vieira <matheus.fvp@outlook.com>
2024-05-28 22:16:55 -0300

    feat: infrastructure layer - category entity to persistence

---
+++ src/main/java/edu/mfvp/javaspringecommerce/infrastructure/persistence/category/entities/CategoryEntity.java
@@ -0,0 +1,29 @@
+package edu.mfvp.javaspringecommerce.infrastructure.persistence.category.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "categories")
+public class CategoryEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+}
