8c82e448a32e059220141800380541bce207b5db
Matheus Vieira <matheus.fvp@outlook.com>
2024-06-05 15:53:17 -0300

    feat: add migrations to product entity

---
+++ src/main/resources/db/migration/V9__INSERT_DATA_INTO_PRODUCTS.sql
@@ -0,0 +1,19 @@
+INSERT INTO products (name, description, price, img_url)
+VALUES
+    ('Smartphone', 'Smartphone Android com tela de 6 polegadas', 499.99, 'https://example.com/smartphone.jpg'),
+    ('Fone de Ouvido Bluetooth', 'Fone de ouvido sem fio com cancelamento de ruído', 79.99, 'https://example.com/fone_de_ouvido.jpg'),
+    ('Livro "A Revolução dos Bichos"', 'Romance clássico de George Orwell', 12.99, 'https://example.com/livro_revolucao_bichos.jpg'),
+    ('Kindle Paperwhite', 'Leitor de e-books com tela iluminada', 129.99, 'https://example.com/kindle_paperwhite.jpg'),
+    ('Camiseta de Algodão', 'Camiseta básica de algodão', 14.99, 'https://example.com/camiseta.jpg'),
+    ('Vestido de Festa', 'Vestido de festa elegante com detalhes em renda', 89.99, 'https://example.com/vestido_festa.jpg'),
+    ('Panela de Pressão Elétrica', 'Panela de pressão elétrica com capacidade para 5 litros', 79.99, 'https://example.com/panela_pressao.jpg'),
+    ('Conjunto de Panelas Antiaderentes', 'Conjunto com 5 panelas antiaderentes', 59.99, 'https://example.com/conjunto_panelas.jpg'),
+    ('Boneca Barbie', 'Boneca Barbie fashion com acessórios', 19.99, 'https://example.com/boneca_barbie.jpg'),
+    ('Bola de Futebol', 'Bola de futebol oficial FIFA', 19.99, 'https://example.com/bola_futebol.jpg'),
+    ('Raquete de Tênis', 'Raquete de tênis profissional com tecnologia avançada', 89.99, 'https://example.com/raquete_tenis.jpg'),
+    ('Shampoo Anti-Caspa', 'Shampoo para controle de caspa e couro cabeludo sensível', 9.99, 'https://example.com/shampoo_anticaspa.jpg'),
+    ('Creme Hidratante Facial', 'Creme hidratante facial com proteção solar FPS 30', 24.99, 'https://example.com/creme_hidratante.jpg'),
+    ('Pneu de Carro', 'Pneu para carros de passeio aro 16', 99.99, 'https://example.com/pneu_carro.jpg'),
+    ('Óleo Lubrificante 10W-40', 'Óleo lubrificante para motores a gasolina e diesel', 29.99, 'https://example.com/oleo_lubrificante.jpg'),
+    ('Vitaminas Multivitamínicas', 'Suplemento de vitaminas e minerais para a saúde geral', 19.99, 'https://example.com/vitaminas.jpg'),
+    ('Café Arábica em Grãos', 'Café arábica de alta qualidade, torrado e moído na hora', 9.99, 'https://example.com/cafe.jpg');
