41f11c1801c7ece540d8676b86513c2602dc5211
Matheus Vieira <matheus.fvp@outlook.com>
2024-06-06 14:14:14 -0300

    relationshsip between product and category entities

--- src/main/java/edu/mfvp/javaspringecommerce/infrastructure/persistence/product/entities/ProductEntity.java
+++ src/main/java/edu/mfvp/javaspringecommerce/infrastructure/persistence/product/entities/ProductEntity.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,7 +34,12 @@ public class ProductEntity {
     private String imgUrl;
 
     @Setter(AccessLevel.NONE)
-    @Transient
+    @ManyToMany
+    @JoinTable(
+        name = "products_categories", 
+        joinColumns = @JoinColumn(name = "product_id"), 
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
     private Set<CategoryEntity> categories = new HashSet<>();
 
     @Override
