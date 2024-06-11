801a202cd5eba9d980bf14329ced4305e41c17a0
Matheus Vieira <matheus.fvp@outlook.com>
2024-06-11 16:57:20 -0300

    feat: create a role entity

---
+++ src/main/java/edu/mfvp/javaspringecommerce/infrastructure/persistence/role/RoleEntity.java
@@ -0,0 +1,42 @@
+package edu.mfvp.javaspringecommerce.infrastructure.persistence.role;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity(name = "roles")
+public class RoleEntity implements Serializable, GrantedAuthority {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleEntity that = (RoleEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+}
